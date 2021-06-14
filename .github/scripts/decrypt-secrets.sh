140449cdb99d7daf0701adb81f13205889a3515d
Julien Ponge <jponge@redhat.com>
2021-06-14 17:12:06 +0200

    Deployment script fixes

--- .github/scripts/decrypt-secrets.sh
+++ .github/scripts/decrypt-secrets.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
 
 echo "Decrypting smallrye signature"
-gpg --quiet --batch --yes --decrypt --passphrase="${SECRET_FILES_PASSPHRASE}" \
+gpg --quiet --batch --yes --decrypt --passphrase="${SECRET_PASSPHRASE}" \
     --output smallrye-sign.asc .github/encrypted/smallrye-sign.asc.gpg
 
 echo "Decrypting Maven settings"
-gpg --quiet --batch --yes --decrypt --passphrase="${SECRET_FILES_PASSPHRASE}" \
+gpg --quiet --batch --yes --decrypt --passphrase="${SECRET_PASSPHRASE}" \
     --output maven-settings.xml .github/encrypted/maven-settings.xml.gpg
---
+++ .github/scripts/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+echo "Set up Git"
+
+git config --global user.name "SmallRye CI"
+git config --global user.email "smallrye@googlegroups.com"
+
+echo "Import key"
+
+gpg --fast-import --no-tty --batch --yes smallrye-sign.asc
+
+echo "Fetch code"
+
+git fetch origin --tags
+git reset --hard
+git checkout main
+
+echo "Deploy"
+
+mvn -B clean deploy -DskipTests -Prelease -s maven-settings.xml
+
