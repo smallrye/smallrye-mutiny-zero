361efcb9d632c4043485a726af152e8f1fb04b8b
Julien Ponge <jponge@redhat.com>
2021-06-15 18:21:04 +0200

    Website CI/CD

--- .github/scripts/render-documentation.sh
+++ .github/scripts/render-documentation.sh
@@ -2,9 +2,10 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-#mvn -f vertx-mutiny-clients/pom.xml pre-site
-#cp -R vertx-mutiny-clients/target/site/apidocs docs/
-#
-#PROJECT_VERSION=$(cat .github/project.yml | yq eval '.release.current-version' -)
-#mike deploy --push --force --update-aliases $PROJECT_VERSION latest
-#mike set-default --push --force latest
+mvn -s .github/maven-ci-settings.xml javadoc:javadoc
+cp -R target/site/apidocs docs/
+
+PROJECT_VERSION=$(cat .github/project.yml | yq eval '.release.current-version' -)
+
+mike deploy --push --force --update-aliases $PROJECT_VERSION latest
+mike set-default --push --force latest
