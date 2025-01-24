5eca19b239e16e9c00e150ce0c3ae6a5e5924317
Julien Ponge <jponge@redhat.com>
2025-01-24 10:24:51 +0100

    build(ci): fix incorrect Maven invocation (#234)

--- .github/scripts/render-documentation.sh
+++ .github/scripts/render-documentation.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-./mvnw -s .github/maven-ci-settings.xml package javadoc:aggregate -DskipTests
+./mvnw package javadoc:aggregate -DskipTests
 cp -R target/reports/apidocs docs/
 
 PROJECT_VERSION=$(cat .github/project.yml | yq eval '.release.current-version' -)
@@ -11,3 +11,4 @@ uv sync --all-extras --dev
 uv run mkdocs build
 uv run mike deploy --push --update-aliases $PROJECT_VERSION latest
 uv run mike set-default --push latest
+
