3ae48282d83a127ae8c7411469b9a7381befba3a
Julien Ponge <jponge@ibm.com>
2026-02-25 10:49:39 +0100

    feat: introduce JSpecify annotations

--- mutiny-zero-flow-adapters/src/main/java/module-info.java
+++ mutiny-zero-flow-adapters/src/main/java/module-info.java
@@ -1,4 +1,6 @@
+@org.jspecify.annotations.NullMarked
 module io.smallrye.mutiny.zero.flow.adapters {
     requires transitive org.reactivestreams;
+    requires transitive org.jspecify;
     exports mutiny.zero.flow.adapters;
 }
